eaba86c5a3b569bd1ba792e480e6e24819ee51ef
Bojki <91488932+bojkip@users.noreply.github.com>
2022-02-28 22:25:29 +0100

    Add files via upload

---
+++ alpha_blog/app/javascript/application.js
@@ -0,0 +1,13 @@
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+
+
+
+import "@hotwired/turbo-rails"
+import "controllers"
+
+import "chartkick"
+import "Chart.bundle"
+import "custom/uni_toggle"
+
+import "popper"
+import "bootstrap"
---
+++ alpha_blog/app/javascript/controllers/position_controller.js
@@ -0,0 +1,11 @@
+// app/javascript/controllers/position_controller.js
+import { Controller } from "@hotwired/stimulus"
+import Sortable from "sortablejs"
+export default class extends Controller {
+
+  connect() {
+    this.sortable = Sortable.create(this.element, {
+      animation: 150
+    })
+  }
+}
