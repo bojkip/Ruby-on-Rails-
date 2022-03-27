21235ec4af8111b197fbad16f96d1fe9800c0664
Bojki <91488932+bojkip@users.noreply.github.com>
2022-03-27 15:03:55 +0200

    Add files via upload

---
+++ alpha_blog4/config/routes.rb
@@ -0,0 +1,19 @@
+Rails.application.routes.draw do
+  #get 'home/index'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+
+  root "home#index"
+
+  resources :articles
+
+  get "signup", to: "users#new"
+  post "users", to: "users#create"
+  resources :users, except: [:new]
+  get "login", to: "sessions#new"
+  post "login", to: "sessions#create"
+  delete "logout", to: "sessions#destroy"
+
+end
