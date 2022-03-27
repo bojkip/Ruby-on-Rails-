057632b2f3f66f7e6a609e7b69acf861440a9053
Bojki <91488932+bojkip@users.noreply.github.com>
2022-03-27 15:02:33 +0200

    Add files via upload

---
+++ alpha_blog4/app/helpers/application_helper.rb
@@ -0,0 +1,13 @@
+module ApplicationHelper
+
+  def gravitar_for(user, options = {size: 80})
+    email_address = user.email.downcase
+    hash = Digest::MD5.hexdigest(email_address)
+    size = options[:size]
+    gravitar_url = "https://www.gravatar.com/avatar/#{hash}?s=#{size}"
+    image_tag(gravitar_url, alt: user.username, class: " pt-3 rounded shadow mx-auto d-block")
+  end
+
+
+
+end
